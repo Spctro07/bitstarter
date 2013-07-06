7ec9b6c43fc284c741196d114ad0e505f95edd70
Ubuntu <ubuntu@ip-172-31-27-148.us-west-2.compute.internal>
2013-07-06 20:33:02 +0000

    Changed source to read html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer();
+var buffer = new Buffer(256);
 buffer.write(fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8'));
